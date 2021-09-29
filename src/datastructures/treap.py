422f079f1e47386cda0ac229e6409bdd72616a89
Giully314 <giully@protonmail.com>
2021-09-29 18:34:00 +0200

    Implementation of right and left rotation in Treap.

--- src/datastructures/treap.py
+++ src/datastructures/treap.py
@@ -11,8 +11,8 @@ class Node:
     left, right, parent: objects of class Node. They can also be None.
     """
 
-    key: Any
-    priority: float
+    _key: Any
+    _priority: float
     _parent: Optional["Node"] = None
     _left: Optional["Node"] = None
     _right: Optional["Node"] = None
@@ -50,5 +50,52 @@ class Node:
 
 @dataclass
 class Treap:
-    root: Node = None
+    _root: Node = None
 
+    
+    def is_root(self, node: Node) -> bool:
+        return node.parent == None
+
+
+    def _right_rotate(self, x: Node):
+        #maybe just return?
+        if x == None or self.is_root(x):
+            raise Exception("Passed node is null or is root.")
+
+        y = x.parent
+        if y.left != x:
+            raise Exception("Right rotation can only be applied to a left child.")
+
+        p = y.parent
+        if p == None:
+            self._root = x
+        else:
+            if p.left == y:
+                p.left = x
+            else:
+                p.right = x
+
+        y.left = x.right
+        x.right = y
+
+
+    def _left_rotate(self, x: Node):
+        #maybe just return?
+        if x == None or self.is_root(x):
+            raise Exception("Passed node is null or is root.")
+
+        y = x.parent
+        if y.right != x:
+            raise Exception("Right rotation can only be applied to a left child.")
+
+        p = y.parent
+        if p == None:
+            self._root = x
+        else:
+            if p.left == y:
+                p.left = x
+            else:
+                p.right = x
+
+        y.right = x.left
+        x.left = y
