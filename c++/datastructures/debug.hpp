101e8db471550ad4be733b81c4a863e8464d662a
Giully314 <giully@protonmail.com>
2021-11-27 11:37:43 +0100

    simple debug macros

---
+++ c++/datastructures/debug.hpp
@@ -0,0 +1,14 @@
+#pragma once 
+
+// #define DEBUG
+
+#ifdef DEBUG
+#define LOG(x)         std::cout << x << std::endl
+#define ENTER(x)       std::cout << "enter " << x << std::endl
+#define EXIT(x)        std::cout << "exit " << s << std::endl
+
+#else
+#define LOG(x)        
+#define ENTER(x)      
+#define EXIT(x)        
+#endif
