eea9bd25f58b08f3b520386825e5d81ed6107b59
Giully314 <giully@protonmail.com>
2021-11-27 11:41:06 +0100

    fixed typo

--- c++/datastructures/debug.hpp
+++ c++/datastructures/debug.hpp
@@ -5,7 +5,7 @@
 #ifdef DEBUG
 #define LOG(x)         std::cout << x << std::endl
 #define ENTER(x)       std::cout << "enter " << x << std::endl
-#define EXIT(x)        std::cout << "exit " << s << std::endl
+#define EXIT(x)        std::cout << "exit " << x << std::endl
 
 #else
 #define LOG(x)        
